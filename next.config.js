f396317154c37611efaf3cabf771f2dc58df7746
David Cheung <davidcheung@live.ca>
2021-01-19 15:28:05 -0500

    assets to be in root folder

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const staging = process.env.NODE_ENV === 'production'
 module.exports = {
-  assetPrefix: staging ? '/commit.dev-proto/' : '',
+  // staging: with github pages CNAME pointed to landing.commit-staging.dev assets should be root as well
+  assetPrefix: staging ? '' : '',
 }
