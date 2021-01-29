76e4f94e28ff02808804de841c9d8383af2283e6
Jason Dippel <dippelj12@gmail.com>
2021-01-28 16:39:40 -0800

    Add testimonials section with carousel

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 export { default as ArticlePreview } from './article-preview'
 export { default as Button } from './button'
+export { default as Testimonials } from './testimonials'
 export { default as Form } from './form'
 export { default as Heading } from './heading'
 export { default as Input } from './input'
