3ec741e1c3f7846e9f24d763948a7dbcc0924ff2
Timothy Rhodes <timothy.rhodes@commit.dev>
2021-02-08 08:24:52 -0800

    fade in / out form, loader and success message

---
+++ components/fade/index.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+const FadeOut = styled.div`
+  display: inline-block;
+  animation: ${(props) => (props.show ? 'none' : 'fadeOut 1s linear')};
+  opacity: ${(props) => (props.show ? 1 : 0)};
+  @keyframes fadeOut {
+    0% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+`
+
+const FadeIn = styled.div`
+  animation: fadeIn 1s linear;
+  opacity: ${(props) => (props.show ? 1 : 0)};
+  display: ${(props) => (props.show ? 'inline-block' : 'none')};
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`
+
+const Fade = styled.div``
+
+export { FadeIn, FadeOut }
