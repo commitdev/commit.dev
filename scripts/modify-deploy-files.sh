609b8c4cbd5e2f4b2517c21ae458c4aff5370570
Jason Dippel <dippelj12@gmail.com>
2021-02-09 15:57:49 -0800

    Cleanup script logging

--- scripts/modify-deploy-files.sh
+++ scripts/modify-deploy-files.sh
@@ -2,5 +2,5 @@
 
 # Removes .html extensions from generated html files as .html extension causes routing issues with current AWS setup
 
-printf "\nRemoving .html extensions... NOT \n"
+printf "\nRemoving .html extensions...\n"
 find ./out -name "*.html" -type f | while read f; do echo "$f --> ${f%.html}"; mv "$f" "${f%.html}"; done
