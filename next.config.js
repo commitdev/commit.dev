aaaf827ff59b96253c07abf955d9a4f1d9de5dc3
Jason Dippel <dippelj12@gmail.com>
2021-02-09 12:05:21 -0800

    Trying trailingSlash field in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // assetPrefix can differ based on NODE_ENV in case its not served from domain root
   assetPrefix: '',
+  trailingSlash: true,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
