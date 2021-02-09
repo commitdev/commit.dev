362116bf4892649bdea6495b8290ad08af1c7a73
Jason Dippel <dippelj12@gmail.com>
2021-02-09 15:16:58 -0800

    Independently push /full page with correct content type and modified name

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   // assetPrefix can differ based on NODE_ENV in case its not served from domain root
   assetPrefix: '',
-  trailingSlash: true,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
--- scripts/modify-deploy-files.sh
+++ scripts/modify-deploy-files.sh
@@ -3,4 +3,4 @@
 # Removes .html extensions from generated html files as .html extension causes routing issues with current AWS setup
 
 printf "\nRemoving .html extensions... NOT \n"
-# find ./out -name "*.html" -type f | while read f; do echo "$f --> ${f%.html}"; mv "$f" "${f%.html}"; done
+find ./out -name "*.html" -type f | while read f; do echo "$f --> ${f%.html}"; mv "$f" "${f%.html}"; done
