ed77cb1cba1af81d53cf2412bd1b8dd39dfcc600
Adrian De Lisle <adriandelisle@gmail.com>
2021-02-23 17:01:42 -0800

    76 created apply page and made apply button go to it (#103)

---
+++ components/layout/content-root.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const ContentRoot = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  min-width: 320px;
+  overflow: hidden;
+  padding: 0px;
+  margin: 0px;
+`
+
+export default ContentRoot
