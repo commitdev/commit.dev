8fb473fe188bd4775a5019f1939dedac4211080d
Adrian De Lisle <adriandelisle@gmail.com>
2021-03-05 14:45:20 -0800

    Fixed the failing e2e test and added an additional step

--- tests/index.e2e.js
+++ tests/index.e2e.js
@@ -1,10 +1,11 @@
 import puppeteer from 'puppeteer'
+
 import { config, PageObjectModel } from '../helpers/e2e'
 
 class LandingPage extends PageObjectModel {
   async checkApplyButtonExists() {
     const buttonText = await this.getTextBySelector({
-      selector: '[data-test-id="button"]',
+      selector: '[data-test-id="applyButton"]',
     })
     await expect(buttonText).toBeTruthy()
   }
@@ -31,6 +32,19 @@ describe('LandingPage', () => {
     await landingPage.checkApplyButtonExists()
     await landingPage.saveScreenshot({
       filename: `${filenamePrefix}_00_landing_page_button`,
+    })
+  })
+
+  it('it goes to the apply page on click of apply button', async () => {
+    await landingPage.clickSelector({
+      selector: '[data-test-id="applyButton"]',
+    })
+    const buttonText = await landingPage.getTextBySelector({
+      selector: '[data-test-id="submitApplicationButton"]',
+    })
+    expect(buttonText).toBeTruthy()
+    await landingPage.saveScreenshot({
+      filename: `${filenamePrefix}_00_landing_page_navigate_to_apply_page`,
     })
   })
 })
