402fe93e289ef48f1d6e68917abc66ed25ae77c4
Alessandro Biavati <alebiavati@users.noreply.github.com>
2021-04-07 15:29:13 -0700

    Fix header to show light variant on pages that are not the home page (#209)

--- components/site-header/index.js
+++ components/site-header/index.js
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import { rem } from 'polished'
 import styled from 'styled-components'
 
@@ -18,9 +19,10 @@ const StyledHeader = styled.header`
   letter-spacing: 0em;
   text-align: left;
   transition: background-color 200ms ease-in-out, padding 200ms ease-in-out;
-  background-color: ${({ isScrolled }) =>
-    isScrolled ? 'rgba(255, 255, 255, 0.5)' : 'rgba(255, 255, 255, 0)'};
-
+  background-color: ${({ variation }) =>
+    variation === COLOR_VARIATIONS.light
+      ? 'rgba(255, 255, 255, 0.5)'
+      : 'rgba(255, 255, 255, 0)'};
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -34,14 +36,24 @@ const StyledHeader = styled.header`
   }
 `
 
+const pagesWithDarkHeroDict = ['/', '/apply']
+
+const usePageWithDarkHero = () => {
+  const router = useRouter()
+  return pagesWithDarkHeroDict.indexOf(router.pathname) !== -1
+}
+
 const SiteHeader = () => {
+  const pageWithDarkHero = usePageWithDarkHero()
   const isScrolledFromTop = useIsScrolledFromTop(10, 40)
-  const colorVariation = isScrolledFromTop
-    ? COLOR_VARIATIONS.light
-    : COLOR_VARIATIONS.dark
+
+  const colorVariation =
+    !pageWithDarkHero || isScrolledFromTop
+      ? COLOR_VARIATIONS.light
+      : COLOR_VARIATIONS.dark
 
   return (
-    <StyledHeader isScrolled={isScrolledFromTop}>
+    <StyledHeader variation={colorVariation} isScrolled={isScrolledFromTop}>
       <Logo variation={colorVariation} />
       <StandardMenu variation={colorVariation} />
       <CompactMenu variation={colorVariation} />
