53978a8896097cb8439975ccb864f1f31f880f13
alexandre-georges <alexandre.georges.pro@gmail.com>
2021-04-09 10:43:38 -0700

    Code review refactoring

--- components/row-container/index.js
+++ components/row-container/index.js
@@ -1,19 +1,13 @@
-import React from 'react'
-
 import { arrayOf, element, string } from 'prop-types'
 import styled from 'styled-components'
 
-const Container = styled.div`
+const RowContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   flex-wrap: wrap;
 `
-
-const RowContainer = ({ className, children }) => (
-  <Container className={className}>{children}</Container>
-)
 
 RowContainer.propTypes = {
   className: string,
