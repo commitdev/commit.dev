ef5aa821837e74aa749c88ff6372550a16fd90d0
Andrew Lukonin <AndrewLukonin@gmail.com>
2021-04-13 12:51:39 -0700

    Revert "code cleanup, fix mobile margin, and multiselect placeholder opacity (#256)" (#261)
    This reverts commit 139fa4b5c917afc0b29091056393bb407e72ab88.

--- pages/apply.js
+++ pages/apply.js
@@ -1,18 +1,81 @@
-import { Form, Page } from 'components'
-import styles from 'styles/apply.module.css'
+import styled from 'styled-components'
+
+import { Heading, Form, Page } from 'components'
+import { MOBILE_SIZE, TABLET_LARGE_SIZE } from 'styles/constants'
+
+const HeroSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  color: #ffffff;
+  background: #000000;
+  background: linear-gradient(248.66deg, #010242 0%, #0f1011 100%);
+  overflow: hidden;
+  flex-grow: 1;
+`
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 1;
+  margin: 0 5rem;
+  @media only screen and (max-width: ${TABLET_LARGE_SIZE}) {
+    margin: 0 3rem;
+  }
+
+  @media only screen and (max-width: ${MOBILE_SIZE}) {
+    margin: 0 1rem;
+  }
+`
+
+const HeroContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  @media only screen and (max-width: ${TABLET_LARGE_SIZE}) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+const HeroHeadingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const HeroHeading = styled(Heading)`
+  max-width: 500px;
+  margin-right: 2rem;
+  line-height: 96px;
+
+  @media only screen and (max-width: ${TABLET_LARGE_SIZE}) {
+    text-align: center;
+
+    line-height: 64px;
+    margin-right: 0px;
+    margin-bottom: 2rem;
+  }
+
+  @media only screen and (max-width: ${MOBILE_SIZE}) {
+    max-width: initial;
+  }
+`
 
 const Apply = () => (
   <Page>
-    <section className={`${styles.heroSection}`}>
-      <div className={`${styles.content}`}>
-        <div className={`${styles.heroContent}`}>
-          <div className={`${styles.heroHeadingContainer}`}>
-            <h1 className={`${styles.heroHeading}`}>Start Taking Control of Your Career</h1>
-          </div>
+    <HeroSection>
+      <Content>
+        <HeroContent>
+          <HeroHeadingContainer>
+            <HeroHeading>Start Taking Control of Your Career</HeroHeading>
+          </HeroHeadingContainer>
           <Form />
-        </div>
-      </div>
-    </section>
+        </HeroContent>
+      </Content>
+    </HeroSection>
   </Page>
 )
 
